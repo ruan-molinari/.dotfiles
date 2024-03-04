692270378ef05f0595fcff66309d6dcf3e275d00
Ruan G. Molinari <110646641+ruan-molinari@users.noreply.github.com>
2024-03-04 16:17:35 -0300

    changint nvim colorscheme to rose pine

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,17 @@
--- Hashmap of themes with { git_repo, theme_name, custom_options }
-local themes = {
-  ['catppuccin'] = { 'catppuccin/nvim', 'catppuccin', { background = { dark = 'mocha' } } },
-  ['gruvbox'] = { 'morhetz/gruvbox', 'gruvbox', {} },
-  ['noctis'] = { 'talha-akram/noctis.nvim', 'noctis', {} },
-  ['tokyodark'] = { 'tiagovla/tokyodark.nvim', 'tokyodark', {} },
-  ['awesome'] = { 'rafi/awesome-vim-colorschemes', 'deus', {} },
-  ['andromeda'] = { 'safv12/andromeda.vim', 'andromeda', {} },
-  ['modest'] = { 'matveyt/vim-modest', 'modest', {} },
-  ['incorrectish'] = { 'incorrectish/incorrectish_colors', 'incorrectish_colors', {} },
-}
-
--- Set the active theme here, only this will be loaded
-local active_theme = 'awesome'
-
 return {
   {
-    themes[active_theme][1],
-    lazy = false,
-    priority = 1000,
-    opts = themes[active_theme][3],
-    config = function ()
-      vim.cmd { cmd = 'colorscheme', args = { themes[active_theme][2] } }
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = 'moon'
+    },
+    config = function (_, opts)
+      require'rose-pine'.setup(opts)
+
+      vim.cmd.colorscheme 'rose-pine'
+      -- uncomment next line for tranrparency
+      -- vim.cmd.highlight 'Normal guibg=none'
     end
-  }
+  },
+
 }
