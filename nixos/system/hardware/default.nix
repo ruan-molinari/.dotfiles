cad4c8cd1759a16ef21ff831f19e956aae41dd32
ruan-molinari <ruan.molinari@proton.me>
2024-05-12 15:24:28 -0300

    Setting xremap for Razer Tartarus

--- nixos/system/hardware/default.nix
+++ nixos/system/hardware/default.nix
@@ -10,36 +10,42 @@
 
   services = {
     xremap = {
-      withHypr = true;
+      withX11 = true;
+      #withGnome = true;
       userName = "ruan";
+
       config = {
         modmap = [
           {
             name = "Razer Tartarus WoW";
+
+            # Remaps must be expressed for QUERTY no matter what keyboard layout is being used
             remap = {
               # First row is by default  | 1 | 2 | 3 | 4 | 5 |
 
               # Second row
-              "KEY_TAB" = "KEY_Q";
-              "KEY_Q"   = "KEY_W";
-              "KEY_W"   = "KEY_F";
-              "KEY_F"   = "KEY_P";
-              "KEY_P"   = "KEY_G";
+              Tab = "Q";
+              Q = "W";
+              W = "E";
+              E = "R";
+              R = "T";
 
               # Third row
-              "KEY_BACKSPACE" = "KEY_A";
-              "KEY_A"         = "KEY_R";
-              "KEY_R"         = "KEY_S";
-              "KEY_S"         = "KEY_T";
-              "KEY_T"         = "KEY_D";
+              CapsLock = "A";
+              A = "S";
+              S = "D";
+              D = "F";
+              F = "G";
 
               # Fourth row
-              "KEY_LEFTSHIFT" = "KEY_Z";
-              "KEY_Z"         = "KEY_X";
-              "KEY_X"         = "KEY_C";
-              "KEY_C"         = "KEY_V";
+              LEFTSHIFT = "Z";
+              Z = "X";
+              X = "C";
+              C = "V";
             };
-            device: "Razer Razer Tartarus Pro";
+            device = {
+              only = "Razer Razer Tartarus Pro";
+            };
           }
         ];
       };
