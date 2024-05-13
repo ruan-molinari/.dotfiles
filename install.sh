26c15fa273befa90db3d535b9115093d293eea67
ruan-molinari <ruan.molinari@proton.me>
2024-05-12 23:36:55 -0300

    Removing kitty

--- install.sh
+++ install.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-echo "Change to home folder"
-cd $HOME
-echo "Clone dotfiles"
-git clone https://github.com/ruan-molinari/dotfiles.git .dotfiles
-echo "Change to dotfiles"
-cd .dotfiles
 echo "Stow dotfiles"
 stow nvim
-stow kitty
 stow nushell
 stow wezterm
-echo "Back to home"\
+echo "Back to home"
 cd ..
